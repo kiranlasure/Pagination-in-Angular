4ad3078b84d99b34c4b365a01a16cfbb7d664158
kiranlasure <kiranlasure95@gmail.com>
2019-12-30 17:27:24 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+ items = [];
+    pageOfItems: Array<any>;
+
+    constructor() { }
+
+    ngOnInit() {
+        // an example array of 150 items to be paged
+        this.items = Array(150).fill(0).map((x, i) => ({ id: (i + 1), name: `Item ${i + 1}`}));
+    }
+
+    onChangePage(pageOfItems: Array<any>) {
+        // update current page of items
+        this.pageOfItems = pageOfItems;
+    }
 }
